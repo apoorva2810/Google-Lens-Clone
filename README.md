8a1b988d604d8e542d27924551cc8724a69ccd03
Apoorva Singh <apoorva.singh2810@gmail.com>
2018-08-17 01:47:27 +0530

    Project Added

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Google-Lens-Clone
 Creating Google Lens Clone using Firebase MLKit (Image Labelling)
+
+## Note:
+
+Use *latest* version of Android Studio ([Android Studio 3.1.3](https://developer.android.com/studio/index.html)).
+
+
+App info:
+
+* compileSdkVersion 28
+* minSdkVersion 23
+* targetSdkVersion 28
---
+++ app/src/main/java/com/apoorvasingh2810/googlelensclone/MainActivity.java
@@ -0,0 +1,13 @@
+package com.apoorvasingh2810.googlelensclone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
